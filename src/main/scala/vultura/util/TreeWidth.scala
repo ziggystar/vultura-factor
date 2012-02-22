051d47696d31964b77513283831e16671250dcdb
Thomas Geier <ziggystar@gmx.de>
2012-02-22 21:31:48 +0100

    more treewidth related methods

--- src/main/scala/vultura/util/TreeWidth.scala
+++ src/main/scala/vultura/util/TreeWidth.scala
@@ -1,6 +1,6 @@
 package vultura.util
 
-import annotation.tailrec
+import java.util.BitSet
 
 /**
  * @author Thomas Geier
@@ -8,13 +8,34 @@ import annotation.tailrec
  */
 
 object TreeWidth {
-  @tailrec
-  def treeWidth[A](cliques: List[Set[A]], ordering: List[A], acc: Int = 0): Int = ordering match {
-    case Nil => acc
-    case h :: t => {
-      val (cliquesWith: List[Set[A]], cliquesWithout) = cliques.partition(_.contains(h))
-      val newClique: Set[A] = cliquesWith.view.flatten.foldLeft(Set.newBuilder[A])(_ += _).result() - h
-      treeWidth(newClique :: cliquesWithout, t, math.max(acc,newClique.size))
+  def treeWidth[A](cliques: Seq[Set[A]], ordering: Seq[A]): Int = {
+    val (elimination,maxCliqueSize) = ordering.foldLeft((cliques,0)){case ((remCliques,max),elimVar) =>
+      val elimResult = eliminateVertex(remCliques,elimVar)
+      (elimResult,math.max(max,elimResult.head.size))
     }
+    assert(elimination.flatten.size == 0, "there was something left after eliminating everything")
+    maxCliqueSize - 1
+  }
+
+  /** places the new elimination clique at the head of the result. */
+  def eliminateVertex[A](cliques: Seq[Set[A]], vertex: A): Seq[Set[A]] = {
+    val (cliquesWith, cliquesWithout) = cliques.partition(_.contains(vertex))
+    val newClique: Set[A] = cliquesWith.view.flatten.toSet - vertex
+    newClique +: cliquesWithout
+  }
+
+  def simplicialEdges[A](cliques: Seq[Set[A]], allVariables: Option[Set[A]] = None): Set[A] = {
+      allVariables.getOrElse(cliques.flatten.toSet).filter{v =>
+      cliques.filter(_.contains(v)).size == 1
+    }
+  }
+
+  def eliminateSimplicials[A](cliques: Seq[Set[A]],allVariables: Option[Set[A]] = None): (Seq[Set[A]],List[A]) = {
+    val simplicialIter: Iterator[(Seq[Set[A]], Seq[A])] =
+      Iterator.iterate((cliques,simplicialEdges((cliques)).toSeq)){case (cliques, simplicials) =>
+        val elminationResult = simplicials.foldLeft(cliques)(eliminateVertex(_,_))
+        (elminationResult,simplicialEdges(elminationResult).toSeq)
+      }.takeWhile(_._2 != Nil)
+    simplicialIter.foldLeft((cliques,List.empty[A])){case ((_,elims),(rem,newElim)) => (rem,elims ++ newElim)}
   }
 }
