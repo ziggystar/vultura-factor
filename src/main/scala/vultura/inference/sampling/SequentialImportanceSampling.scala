2668ee7b033f00f1140b5c94d2cfd611d73e5124
Thomas Geier <thomas.geier@uni-ulm.de>
2012-02-24 17:03:39 +0100

    small refactoring

--- src/main/scala/vultura/inference/sampling/SequentialImportanceSampling.scala
+++ src/main/scala/vultura/inference/sampling/SequentialImportanceSampling.scala
@@ -4,8 +4,7 @@ import vultura.factors._
 import util.Random
 import collection.mutable.WrappedArray
 import scalaz.Monoid
-import vultura.util.{AbelianGroup, RingWithZero, Measure}
-import collection.Seq
+import vultura.util.RingWithZero
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,7 +13,7 @@ import collection.Seq
  */
 
 object SequentialImportanceSampling {
-  /** @return a set of samples and associated weights. */
+  /** @return a set of samples and associated weights.    */
   def weightedImportanceSampling[A](factorSequence: Seq[Seq[A]], numSamples: Int, ring: RingWithZero[Double], random: Random)(implicit ev: Factor[A,Double]): ParticleSeq[Double] = {
     //draw initial samples
     val initialFactor: ProductFactor[A, Double] = ProductFactor(factorSequence.head, ring.multiplication)
@@ -25,15 +24,15 @@ object SequentialImportanceSampling {
       implicitly[Monoid[Double]]
     )
     //weights are correct for initial samples
-    val particles = factorSequence.tail.foldLeft(initialParticles){ case(particles,newFactors) =>
+    val particles = factorSequence.tail.foldLeft(initialParticles){ case(remainingParticles,newFactors) =>
       //extend a particle x by drawing from newFactors | x exactly; weight remains unchanged,
       //multiply weight by partition function over new factors (probability of old sample given new distribution)
       val newFactor = ProductFactor(newFactors, ring.multiplication)
       val unweightedParticles: Seq[(WrappedArray[Int], Double)] = for(
-        (oldAssignment,oldWeights) <- particles.particles.toSeq;
+        (oldAssignment,oldWeights) <- remainingParticles.particles.toSeq;
         oldWeight <- oldWeights
       ) yield {
-        val conditionedNewFactor: ProductFactor[A, Double] = newFactor.condition(particles.variables,oldAssignment.toArray)
+        val conditionedNewFactor: ProductFactor[A, Double] = newFactor.condition(remainingParticles.variables,oldAssignment.toArray)
         val conditionedPartition = partition(conditionedNewFactor,ring.addition)
         val sampleExtension = sample(conditionedNewFactor,random)
         val newParticle = oldAssignment ++ sampleExtension
@@ -43,10 +42,10 @@ object SequentialImportanceSampling {
       //renormalized
       val normalizedParticles: Seq[(Array[Int], Double)] = unweightedParticles.map(t => (t._1.toArray,t._2/normalizingConstant))
       ParticleSeq(
-        (particles.variables ++ variables(newFactor)).distinct,
-        (particles.domains ++ domains(newFactor)).distinct,
+        (remainingParticles.variables ++ variables(newFactor)).distinct,
+        (remainingParticles.domains ++ domains(newFactor)).distinct,
         ParticleSeq.particleSeqToMap(normalizedParticles),
-        particles.sumMonoid)
+        remainingParticles.sumMonoid)
     }
 
     particles
