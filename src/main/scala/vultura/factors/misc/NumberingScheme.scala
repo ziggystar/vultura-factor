0bf13b563be9dbe536fd8c3fa4c37d7711b5f42f
Thomas Geier <ziggystar@gmx.de>
2012-02-29 21:09:36 +0100

    work on filtering

---
+++ src/main/scala/vultura/factors/misc/NumberingScheme.scala
@@ -0,0 +1,20 @@
+package vultura.factors.misc
+
+import collection.Seq
+
+/**
+ * @author Thomas Geier
+ * @since 29.02.12
+ */
+
+case class NumberingScheme[A](numbering: Map[A,Int], nextFree: Int) {
+  def register(elem: A*): NumberingScheme[A] = {
+    val newElems = elem.filterNot(numbering.contains)
+    val newEntries: Seq[(A, Int)] = newElems.zipWithIndex.map(t => (t._1,t._2 + nextFree))
+    NumberingScheme(numbering ++ newEntries, nextFree + newElems.size)
+  }
+}
+
+object NumberingScheme {
+  def empty[A]: NumberingScheme[A] = NumberingScheme[A](Map.empty,0)
+}
