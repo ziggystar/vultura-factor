730309b1a389a6e252186498f9a99250c7eb8380
Thomas Geier <thomas.geier@uni-ulm.de>
2012-03-15 18:45:30 +0100

    fix reading of multiple mlns (dirty)

--- src/main/scala/vultura/util/TreeWidth.scala
+++ src/main/scala/vultura/util/TreeWidth.scala
@@ -12,7 +12,7 @@ import annotation.tailrec
 object TreeWidth {
   def treeWidth[A](cliques: Seq[Set[A]], ordering: Seq[A]): Int = {
     val (elimination,maxCliqueSize) = ordering.foldLeft((cliques,0)){case ((remCliques,max),elimVar) =>
-      val elimResult = eliminateVertex(remCliques,elimVar)
+      val elimResult = eliminateVertex(remCliques,elimVar)._1
       (elimResult,math.max(max,elimResult.head.size))
     }
     assert(elimination.flatten.size == 0, "there was something left after eliminating everything")
@@ -20,10 +20,10 @@ object TreeWidth {
   }
 
   /** places the new elimination clique at the head of the result. */
-  def eliminateVertex[A](cliques: Seq[Set[A]], vertex: A): Seq[Set[A]] = {
+  def eliminateVertex[A](cliques: Seq[Set[A]], vertex: A): (Seq[Set[A]],Int) = {
     val (cliquesWith, cliquesWithout) = cliques.partition(_.contains(vertex))
     val newClique: Set[A] = cliquesWith.view.flatten.toSet - vertex
-    newClique +: cliquesWithout
+    (newClique +: cliquesWithout, newClique.size)
   }
 
   def simplicialEdges[A](cliques: Seq[Set[A]], allVariables: Option[Set[A]] = None): Set[A] = {
@@ -35,7 +35,7 @@ object TreeWidth {
   def eliminateSimplicials[A](cliques: Seq[Set[A]],allVariables: Option[Set[A]] = None): (Seq[Set[A]],List[A]) = {
     val simplicialIter: Iterator[(Seq[Set[A]], Seq[A])] =
       Iterator.iterate((cliques,simplicialEdges((cliques)).toSeq)){case (cliques, simplicials) =>
-        val elminationResult = simplicials.foldLeft(cliques)(eliminateVertex(_,_))
+        val elminationResult = simplicials.foldLeft(cliques)(eliminateVertex(_,_)._1)
         (elminationResult,simplicialEdges(elminationResult).toSeq)
       }.takeWhile(_._2 != Nil)
     simplicialIter.foldLeft((cliques,List.empty[A])){case ((_,elims),(rem,newElim)) => (rem,elims ++ newElim)}
@@ -48,16 +48,19 @@ object TreeWidth {
   }
 
   @tailrec
-  def minDegreeOrdering(cliques: Seq[Set[Int]], acc: List[Int] = Nil): List[Int] = {
+  def minDegreeOrderingAndWidth(cliques: Seq[Set[Int]], acc: List[Int] = Nil, maxSize: Int = 0): (List[Int],Int) = {
     val vertices = cliques.flatten.distinct
     if (vertices.isEmpty) {
-      acc.reverse
+      (acc.reverse, maxSize)
     } else {
       def degree(v: Int): Int = cliques.foldLeft(Set[Int]()) {
         case (in, next) => if (next(v)) in union next else in
       }.size - 1
       val minDegreeVertex = vertices.minBy(degree)
-      minDegreeOrdering(eliminateVertex(cliques, minDegreeVertex), minDegreeVertex :: acc)
+      val (elimRest,cliqueSize) = eliminateVertex(cliques, minDegreeVertex)
+      minDegreeOrderingAndWidth(elimRest, minDegreeVertex :: acc, (maxSize max cliqueSize))
     }
   }
+
+  def minDegreeOrdering(cliques: Seq[Set[Int]]): List[Int] = minDegreeOrderingAndWidth(cliques)._1
 }
