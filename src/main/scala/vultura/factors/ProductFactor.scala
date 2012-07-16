15dff8f9fb5de16cc3a1443c962f0760171b6dd1
Thomas Geier <thomas.geier@uni-ulm.de>
2012-07-16 16:51:55 +0200

    fix merge

--- src/main/scala/vultura/factors/ProductFactor.scala
+++ src/main/scala/vultura/factors/ProductFactor.scala
@@ -12,9 +12,9 @@ import collection.immutable.Stream
  * @since 05.02.12
  */
 
-case class ProductFactor[T,R](_factors: Seq[T],
-                              productMonoid: Monoid[R])
-                             (implicit val fev: Factor[T,R], cm: ClassManifest[R]) {
+class ProductFactor[T,R](_factors: Seq[T],
+                         val productMonoid: Monoid[R])
+                        (implicit val fev: Factor[T,R], val cm: ClassManifest[R]) {
   val factors = _factors.toIndexedSeq
   val variables: Array[Int] = factors.flatMap(f => vf.variables(f)).toSeq.distinct.toArray
   val domains: Array[Array[Int]] = variables.map(factors.flatMap(f => vf.variables(f).zip(vf.domains(f))).toMap)
@@ -45,25 +45,15 @@ case class ProductFactor[T,R](_factors: Seq[T],
     result
   }
 
-  def condition(variables: Array[Int], value: Array[Int]): ProductFactor[T,R] = {
-    val zipped = variables zip value
-    val reducedFactors = for(
-      f <- factors;
-      fv = vf.variables(f);
-      //take only
-      (fvars,fvals) = zipped.filter(t => fv.contains(t._1)).unzip
-    ) yield vf.condition(f,fvars.toArray,fvals.toArray)
-    val (constant,varying) = reducedFactors.partition(vf.variables(_).size == 0)
-    val constantNotZero = constant.filterNot(f => vf.evaluate(f,Array()) == productMonoid.zero)
-    ProductFactor(varying.toIndexedSeq ++ constantNotZero,productMonoid)
-  }
-
   def conditionView(variables: Array[Int], value: Array[Int]): ProductFactor[FactorView[T],R] = ProductFactor(factors.map(new FactorView(variables zip value toMap, _)),productMonoid)
 
   def filter(p: T => Boolean): ProductFactor[T, R] = ProductFactor(factors.filter(p),productMonoid)
 
   /** @return Partition function and `numSamples` samples or `None` if partition is zero. */
-  def partitionAndSample(random: Random, measure: Measure[R], numSamples: Int)(implicit cm: ClassManifest[R]): Option[(R,IndexedSeq[Array[Int]])] = {
+  def partitionAndSample(random: Random,
+                         measure: Measure[R],
+                         numSamples: Int)(implicit cm: ClassManifest[R],
+                                          conditionableT: Conditionable[T]): Option[(R,IndexedSeq[Array[Int]])] = {
     val sumMonoid = measure.sum
     if(this.factors.isEmpty)
       return Some((this.productMonoid.zero, IndexedSeq.fill(numSamples)(Array.empty[Int])))
@@ -103,7 +93,7 @@ case class ProductFactor[T,R](_factors: Seq[T],
           //condition on sample so far; this could lead to factors becoming constant and thus variables becoming independent
           //thus we need to track these variables and sample them in an additional step
           val priorVariablesAndDomains: Array[(Int, Array[Int])] = eliminationClique.variables zip eliminationClique.domains
-          val conditioned: ProductFactor[Either[T, TableFactor[R]], R] = eliminationClique.condition(sampleVariables,sampleValues)
+          val conditioned: ProductFactor[Either[T, TableFactor[R]], R] = condition(eliminationClique,sampleVariables,sampleValues)
           val extensionVariables = vf.variables(conditioned)
 
           val extensionValues: Array[Int] = if(extensionVariables.size > 0)
