a486996faf62c1d3a9e1b4bd74cb64507df7e649
Thomas Geier <thomas.geier@uni-ulm.de>
2013-06-11 13:50:37 +0200

    - refactor BP code into a class

--- src/main/scala/vultura/fastfactors/BeliefPropagation.scala
+++ src/main/scala/vultura/fastfactors/BeliefPropagation.scala
@@ -8,47 +8,51 @@ import scala.util.Random
  * User: Thomas Geier
  * Date: 6/10/13
  */
-object BeliefPropagation {
+class BeliefPropagation(factors: IndexedSeq[FastFactor], domains: Array[Int], ring: RingZ[Double]) {
+  private val cg = BeliefPropagation.createBetheClusterGraph(factors,domains,ring)
+  private val messages: mutable.HashMap[(Int,Int),FastFactor] =
+    cg.sepsets.map{ case (edge,vars) => edge -> FastFactor.uniform(vars,domains,ring)}(collection.breakOut)
 
-  def loopyBeliefPropagation(factors: IndexedSeq[FastFactor], ring: RingZ[Double], domains: Array[Int], maxiter: Int = 1000, tol: Double = 1e-5): Double = {
-    val cg = createBetheClusterGraph(factors,domains,ring)
-    val messages: mutable.HashMap[(Int,Int),FastFactor] =
-      cg.sepsets.map{ case (edge,vars) => edge -> FastFactor.uniform(vars,domains,ring)}(collection.breakOut)
 
-    /** Computes the standard update:
-      * $$\delta'_{i-j} \propto \Sum_{C_i - S_{i,j}} \Psi_i \cdot \Prod_{k\in(N_i - \{i\}} \delta_{k - j}$$.
-      *
-      * @return The residual of the update. */
-    def updateMessage(edge: (Int,Int)): Double = {
-      val (from,to) = edge
-      //multiply all incoming messages to `from` without that from `to` with `from`'s factor
-      val newFactor = FastFactor.multiplyRetain(ring)(domains)(
-        factors = cg.neighbours(from)
-          .filterNot(_ == to)
-          .map(fromNeighbour => messages((fromNeighbour,from))) :+ cg.clusterFactors(from),
-        cg.sepsets(edge)
-      )
-      //mutate message
-      //TODO use sumProduct message directly and write to existing array?
-      val residual = ring.maxNorm(messages(edge).values,newFactor.values)
-      messages(edge) = newFactor.normalize(ring)
-      residual
-    }
+  /** Computes the standard update:
+    * $$\delta'_{i-j} \propto \Sum_{C_i - S_{i,j}} \Psi_i \cdot \Prod_{k\in(N_i - \{i\}} \delta_{k - j}$$.
+    *
+    * @return The residual of the update. */
+  private def updateMessage(edge: (Int,Int)): Double = {
+    val (from,to) = edge
+    //multiply all incoming messages to `from` without that from `to` with `from`'s factor
+    val newFactor = FastFactor.multiplyRetain(ring)(domains)(
+      factors = cg.neighbours(from)
+        .filterNot(_ == to)
+        .map(fromNeighbour => messages((fromNeighbour,from))) :+ cg.clusterFactors(from),
+      cg.sepsets(edge)
+    )
+    //mutate message
+    //TODO use sumProduct message directly and write to existing array?
+    val residual = ring.maxNorm(messages(edge).values,newFactor.values)
+    messages(edge) = newFactor.normalize(ring)
+    residual
+  }
 
+  private var iterations = 0
+  def run(maxiter: Int = 1000, tol: Double = 1e-10) {
     val randomOrder: IndexedSeq[(Int, Int)] = Random.shuffle(messages.keys.toIndexedSeq)
-    var iterations = 0
     var maxresidual = Double.PositiveInfinity
     while(iterations <= maxiter && maxresidual > tol){
       maxresidual = randomOrder.map(updateMessage).max
       iterations += 1
     }
+  }
 
-    def clusterBelief(ci: Int): FastFactor = FastFactor.multiplyRetain(ring)(domains)(
-      factors = cg.neighbours(ci).map(from => messages((from,ci))) :+ cg.clusterFactors(ci),
-      cg.clusterFactors(ci).variables).normalize(ring)
+  def clusterBelief(ci: Int): FastFactor = FastFactor.multiplyRetain(ring)(domains)(
+    factors = cg.neighbours(ci).map(from => messages((from,ci))) :+ cg.clusterFactors(ci),
+    cg.clusterFactors(ci).variables).normalize(ring)
 
+
+  def logZ: Double = {
     def expectation(p: Array[Double], f: Array[Double]) = p.zip(f).map(t => t._1 * t._2).sum
     def entropy(ps: Array[Double]) = -(for (p <- ps) yield p * math.log(p)).sum
+
     val clusterExpectation = cg.clusterFactors.zipWithIndex
       .map{case (f, cI) => expectation(ring.normalRepresentation(clusterBelief(cI).values),ring.normalRepresentation(f.values).map(math.log))}
 
@@ -61,8 +65,10 @@ object BeliefPropagation {
 
     clusterExpectation.sum + clusterEntropies.sum - variableEntropies.sum
   }
+}
 
 
+object BeliefPropagation{
   def createBetheClusterGraph(factors: IndexedSeq[FastFactor],domains: Array[Int], ring: RingZ[Double]): ClusterGraph  = {
     //we append a cluster for each variable after the factor clusters
     val variables = factors.flatMap(_.variables).toSet.toArray
@@ -79,18 +85,17 @@ object BeliefPropagation {
       sepsets = vars2factors.flatMap{case (v,fs) => fs.map(f => (v + variableShift,f) -> Array(v)) ++ fs.map(f => (f,v + variableShift) -> Array(v))}
     )
   }
+}
 
-  /**
-   *
-   * @param clusters `clusters(i)` contains all variables in cluster `i`.
-   * @param clusterFactors Contains a factor for each cluster, or `None`, if the neutral factor shall be assumed.
-   * @param neighbours `neighbours(i)` contains the indices of all neighbouring clusters.
-   * @param sepsets `sepsets((i,j))` contains all variables, that are shared over edge `(i,j)`, for some connected
-   *               clusters `i` and `j`.
-   */
-  case class ClusterGraph(clusterFactors: IndexedSeq[FastFactor],
-                          neighbours: Array[Array[Int]],
-                          sepsets: Map[(Int,Int),Array[Int]]){
-    def edges: Iterable[(Int,Int)] = for((sinks,srcI) <- neighbours.zipWithIndex; sinkI <- sinks) yield (srcI,sinkI)
-  }
+/**
+ *
+ * @param clusterFactors Contains a factor for each cluster, or `None`, if the neutral factor shall be assumed.
+ * @param neighbours `neighbours(i)` contains the indices of all neighbouring clusters.
+ * @param sepsets `sepsets((i,j))` contains all variables, that are shared over edge `(i,j)`, for some connected
+ *               clusters `i` and `j`.
+ */
+case class ClusterGraph(clusterFactors: IndexedSeq[FastFactor],
+                        neighbours: Array[Array[Int]],
+                        sepsets: Map[(Int,Int),Array[Int]]){
+  def edges: Iterable[(Int,Int)] = for((sinks,srcI) <- neighbours.zipWithIndex; sinkI <- sinks) yield (srcI,sinkI)
 }
--- src/test/scala/vultura/fastfactors/BeliefPropagation$Test.scala
+++ src/test/scala/vultura/fastfactors/BeliefPropagation$Test.scala
@@ -3,7 +3,6 @@ package vultura.fastfactors
 import org.specs2.Specification
 import org.specs2.specification.Fragments
 import Utils._
-import BeliefPropagation._
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,20 +10,26 @@ import BeliefPropagation._
  * Date: 6/10/13
  */
 class BeliefPropagation$Test extends Specification {
+  def BpLogZ(factors: IndexedSeq[FastFactor], ring: RingZ[Double], domains: Array[Int]): Double = {
+    val bp = new BeliefPropagation(factors,domains,ring)
+    bp.run(10000,1e-10)
+    bp.logZ
+  }
+
   def is: Fragments =
   "constructing bethe graph" ^
     "from one factor over single variable" ^
       "must have correct neighbours" !
-        (createBetheClusterGraph(IS(FF(AI(0),AD(1,2))),AI(2),NormalD).neighbours.deep === AAI(AI(1),AI(0)).deep) ^
+        (BeliefPropagation.createBetheClusterGraph(IS(FF(AI(0),AD(1,2))),AI(2),NormalD).neighbours.deep === AAI(AI(1),AI(0)).deep) ^
       "must have correct neighbours" !
-        (createBetheClusterGraph(IS(FF(AI(0),AD(1,2))),AI(2),NormalD).neighbours.deep === AAI(AI(1),AI(0)).deep) ^
+        (BeliefPropagation.createBetheClusterGraph(IS(FF(AI(0),AD(1,2))),AI(2),NormalD).neighbours.deep === AAI(AI(1),AI(0)).deep) ^
       "must have correct sepsets" !
-        (createBetheClusterGraph(IS(FF(AI(0),AD(1,2))),AI(2),NormalD).sepsets.mapValues(_.toSeq) === Map((0,1) -> Seq(0),(1,0) -> Seq(0))) ^
+        (BeliefPropagation.createBetheClusterGraph(IS(FF(AI(0),AD(1,2))),AI(2),NormalD).sepsets.mapValues(_.toSeq) === Map((0,1) -> Seq(0),(1,0) -> Seq(0))) ^
       p^
   p^
   "tests of BP" ^
   "BP on two independent variables" !
-    (loopyBeliefPropagation(IS(FF(AI(0),AD(1,2)),FF(AI(1),AD(3,4))),NormalD,Array(2,2),1000,1e-9) must beCloseTo(math.log(21d),1e-5)) ^
+    (BpLogZ(IS(FF(AI(0),AD(1,2)),FF(AI(1),AD(3,4))),NormalD,Array(2,2)) must beCloseTo(math.log(21d),1e-8)) ^
   "BP on one var with two factors" !
-    (loopyBeliefPropagation(IS(FF(AI(0),AD(1,2)),FF(AI(0),AD(3,4))),NormalD,Array(2)) must beCloseTo(math.log(11d),1e-5))
+    (BpLogZ(IS(FF(AI(0),AD(1,2)),FF(AI(0),AD(3,4))),NormalD,Array(2)) must beCloseTo(math.log(11d),1e-8))
 }
