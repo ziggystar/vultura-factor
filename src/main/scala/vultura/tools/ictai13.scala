55765ae1e77268ad2879fa4ebe7303ec75054b80
Thomas Geier <ziggystar@gmx.de>
2013-06-16 14:55:00 +0200

    - work on experiment support

--- src/main/scala/vultura/tools/ictai13.scala
+++ src/main/scala/vultura/tools/ictai13.scala
@@ -61,6 +61,10 @@ object ictai13 {
       descr = "number of steps the algorithm shall perform",
       default = Some(10)
     )
+    val dontPrintProblem = opt[Boolean](
+      name = "no-print-problem",
+      descr = "output each generated problem in uai format"
+    )
   }
 
   /*
@@ -71,20 +75,42 @@ object ictai13 {
     val config = new Config(args)
 
     val generator = generateFromString(config.problemCfg()).fold(msg => sys.error("could not parse problem descriptor:\n" + msg), identity)
+    val experiment: Experiment = ???
+    val problemReporter: ExperimentReporter[(String,Long,Problem)] = ???
     for{
-      problemSeed <- config.problemSeed() until (config.problemSeed() + config.problemCount())
+      problemSeed <- config.problemSeed() until config.problemSeed() + config.problemCount()
       problem = generator(problemSeed)
-      _ = printProblem(problem)
-      groundTruth = createGroundTruth(problem)
-      algorithmSeed <- config.algorithmSeed() until (config.algorithmSeed() + config.algorithmRuns())
+      _ = printProblem(problem, !config.dontPrintProblem())
+      experimentSeed <- config.algorithmSeed() until config.algorithmSeed() + config.algorithmRuns()
     }{
-      val reporter = new Reporter(config.problemCfg(),problemSeed,problem,config.algorithmCfg(),algorithmSeed,groundTruth)
-      run(config.algorithmCfg(), problem, algorithmSeed, config.algorithmSteps(),reporter)
+      import ExperimentReporter._
+      var run = experiment.create(problem,experimentSeed)
+      val reporter: ExperimentReporter[(Int, experiment.State)] =
+        problemReporter.freeze((config.problemCfg(),problemSeed,problem)) + experiment.reporter
+      println(reporter.header)
+      var iteration = 1
+      var converged = false
+      do {
+        val (nextState,converged2) = experiment.step(run)
+        converged = converged2
+        run = nextState
+        println(reporter.experiment((iteration,run)))
+        iteration += 1
+      } while(!converged)
     }
   }
 
-  def printProblem(p: Problem){
-    println(p.uaiString)
+  trait Experiment{
+    type State
+    def create(p: Problem, seed: Long): State
+    /** @return Next experiment state and true if it shall be continued. */
+    def step(a: State): (State,Boolean)
+    def reporter: ExperimentReporter[(Int,State)]
+  }
+
+  def printProblem(p: Problem, print: Boolean){
+    if(print)
+      println(p.uaiString)
   }
 
   /** Provides ln(Z) and variable marginals. */
@@ -93,21 +119,9 @@ object ictai13 {
     (jt.logZ,jt.decodedVariableBelief)
   }
 
-  def run(config: String, p: Problem, seed: Long, iterations: Int, reporter: Reporter) {
-    val alg = createAlgorithm(config,p,seed)
-    reporter.generateHeader
-    var iteration = 1
-    var converged = false
-    do {
-      converged = alg.step()
-      reporter.generateReport(alg,iteration)
-      iteration += 1
-    }while(iterations <= iterations && !converged)
-  }
-
   def createAlgorithm(config: String, p: Problem, seed: Long): InfAlg with ConvergingStepper[Unit] =
-//    new CBP(p,new Random(seed),CBP.leafSelectionSlowestSettler,CBP.variableSelectionSlowestSettler)
-      new BeliefPropagation(p,new Random(seed))
+    new CBP(p,new Random(seed),CBP.leafSelectionSlowestSettler,CBP.variableSelectionSlowestSettler,10,1e-5)
+//      new BeliefPropagation(p,new Random(seed))
 
   class Reporter(graphConf: String, graphSeed: Long, problem: Problem, algConf: String, algSeed: Long, truth: (Double, Int => FastFactor)){
     val startTime = ManagementFactory.getThreadMXBean.getCurrentThreadCpuTime
@@ -167,4 +181,42 @@ object ictai13 {
     margStatistics(problem,trueMargs,estimate)(FastFactor.maxDiff)(_.mean)
   def meanSquareDiff(problem: Problem, trueMargs: Int => FastFactor, estimate: Int => FastFactor): Double =
     margStatistics(problem,trueMargs,estimate){case (f1,f2) => math.pow(FastFactor.maxDiff(f1,f2),2)}(_.mean)
+
+  def meanDiffReporter(truth: InfAlg) =  ExperimentReporter[InfAlg]("diff.mean",
+    ia => meanDiff(truth.getProblem,truth.decodedVariableBelief,ia.decodedVariableBelief).toString)
+  def meanKLReporter(truth: InfAlg) =  ExperimentReporter[InfAlg]("kl.mean",
+      ia => meanKL(truth.getProblem,truth.decodedVariableBelief,ia.decodedVariableBelief).toString)
+  def maxDiffReporter(truth: InfAlg) =  ExperimentReporter[InfAlg]("diff.max",
+      ia => maxDiff(truth.getProblem,truth.decodedVariableBelief,ia.decodedVariableBelief).toString)
+  def meanSquaredDiffReporter(truth: InfAlg) =  ExperimentReporter[InfAlg]("squarediff.mean",
+      ia => meanSquareDiff(truth.getProblem,truth.decodedVariableBelief,ia.decodedVariableBelief).toString)
+}
+
+trait ExperimentReporter[-A]{
+  def separator: String = "\t"
+  def prefix: String = "*"
+  def header: String
+  def experiment(a: A): String
+  def +[B](other: ExperimentReporter[B]) = new ExperimentReporter[(A,B)]{
+      def header: String = this.header + separator + other.header
+      def experiment(ab: (A,B)): String = ExperimentReporter.this.experiment(ab._1) + separator + other.experiment(ab._2)
+    }
+  def also[B <: A](other: ExperimentReporter[B]) = new ExperimentReporter[B]{
+      def header: String = this.header + separator + other.header
+      def experiment(a: B): String = ExperimentReporter.this.experiment(a) + separator + other.experiment(a)
+    }
+  def comap[B](f: B => A): ExperimentReporter[B] = new ExperimentReporter[B] {
+    def experiment(a: B): String = ExperimentReporter.this.experiment(f(a))
+    def header: String = ExperimentReporter.this.header
+  }
+  def freeze(a: A): ExperimentReporter[Unit] = comap(Unit => a)
+}
+
+object ExperimentReporter{
+  def apply[A](head: String, report: A => String) = new ExperimentReporter[A] {
+    def header: String = head
+    def experiment(a: A): String = report(a)
+  }
+  implicit def absorbUnit1[A](r: ExperimentReporter[(Unit,A)]): ExperimentReporter[A] = r.comap((Unit,_))
+  implicit def absorbUnit2[A](r: ExperimentReporter[(A,Unit)]): ExperimentReporter[A] = r.comap((_,Unit))
 }
