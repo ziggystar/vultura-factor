62fe452e4a970586aa5d5588fac038d4d65a3432
Thomas Geier <thomas.geier@uni-ulm.de>
2013-11-26 18:29:15 +0100

    Merge branch 'rg-inference'
    Conflicts:
    	build.sbt
    	src/test/scala/vultura/fastfactors/LogDTest.scala

--- src/test/scala/vultura/fastfactors/LogDTest.scala
+++ src/test/scala/vultura/fastfactors/LogDTest.scala
@@ -10,7 +10,7 @@ import Utils._
  * User: Thomas Geier
  * Date: 4/9/13
  */
-class LogDTest extends Specification {
+class LogDTest extends Specification with ScalaCheck {
   import math.{log,exp}
 
   val distribution = Gen.containerOf[Vector,Double](Gen.posNum[Double])
