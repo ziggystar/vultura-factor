1e6b85279437a2ca2b584f4ba33febd14c475666
Thomas Geier <thomas.geier@uni-ulm.de>
2014-03-05 14:01:26 +0100

    implement BP using Calibrator

---
+++ src/test/scala/vultura/fastfactors/algorithms/calibration/BP_CalTest.scala
@@ -0,0 +1,20 @@
+package vultura.fastfactors.algorithms.calibration
+
+import org.specs2.Specification
+import vultura.fastfactors.generators._
+import scala.util.Random
+import org.specs2.specification.Fragments
+import vultura.fastfactors.FastFactorMatchers
+import vultura.fastfactors.algorithms.{BeliefPropagation, CalibratedJunctionTree}
+
+/**
+ * @author Thomas Geier <thomas.geier@uni-ulm.de>
+ */
+class BP_CalTest extends Specification with FastFactorMatchers{
+  val treeProb = treeK(10,3,2,expGauss(1),new Random(0))
+
+  override def is: Fragments =
+    "must be exact on tree problem" ! {
+      new BP_Cal(treeProb).logZ must beCloseTo(new CalibratedJunctionTree(treeProb).logZ, 0.01)
+    }
+}
