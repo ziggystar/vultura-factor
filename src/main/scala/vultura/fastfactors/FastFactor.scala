d3b76204ee1babfb3d13b30ac61adafcef828df8
Thomas Geier <thomas.geier@uni-ulm.de>
2014-03-21 16:58:52 +0100

    add some methods to FF and Problem

--- src/main/scala/vultura/fastfactors/FastFactor.scala
+++ src/main/scala/vultura/fastfactors/FastFactor.scala
@@ -71,6 +71,12 @@ case class FastFactor(variables: Array[Int], values: Array[Double]){
   override def toString: String = f"FastFactor(VAR: ${variables.mkString(",")},VAL: ${values.mkString(",")})"
   def toBriefString: String = f"${variables.mkString(",")} | ${values.map("%.2f".format(_)).mkString(",")}"
   def map(f: Double => Double): FastFactor = this.copy(values = values.map(f))
+
+  def eval(values: Array[Val], domains: Array[Int]): Double = {
+    val index = values.zip(variables.map(domains)).foldLeft((0,1)){
+      case ((acc,stride),(value,domainSize)) => (acc + value * stride, stride * domainSize)}._1
+    this.values(index)
+  }
 }
 
 object FastFactor{
