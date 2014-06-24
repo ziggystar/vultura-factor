1207f2c8c76bd27915bada179e6b16104bd05de1
Thomas Geier <thomas.geier@uni-ulm.de>
2014-06-24 14:13:41 +0200

    add mutable array-based queue

---
+++ src/main/scala/vultura/util/MutableArrayQueue.scala
@@ -0,0 +1,69 @@
+package vultura.util
+
+import scala.collection.mutable
+
+/**
+ * Mutable Queue backed by an ArraySeq. Grows on demand.
+ */
+class MutableArrayQueue[T](initialSize: Int) extends IndexedSeq[T] {
+  /** Here begins the content. */
+  private var start: Int = 0
+  /** Here ends the content (exclusively). */
+  private var end: Int = 0
+  private var data: mutable.ArraySeq[T] = new mutable.ArraySeq(initialSize + 1)
+  def capacity: Int = data.size - 1
+  /** Increment a pointer into the array.*/
+  @inline
+  private def incP(i: Int): Int = (i + 1) % data.size
+  def enqueue(t: T): Unit = {
+    if(size < capacity) {
+      data(end) = t
+      end = incP(end)
+    } else {
+      val newData = new mutable.ArraySeq[T](data.size*2)
+      var to = 0
+      while(start != end){
+        newData(to) = data(start)
+        to = to + 1
+        start = incP(start)
+      }
+      newData(to) = t
+      data = newData
+      start = 0
+      end = incP(end)
+    }
+  }
+  /** @throws RuntimeException if the queue is empty. */
+  def dequeue(): T = {
+    if(start == end)
+      throw new NoSuchElementException("trying to dequeue empty queue")
+    else {
+      val r = data(start)
+      start = incP(start)
+      r
+    }
+  }
+
+  override def size: Int = {
+    val r = end - start
+    if(r >= 0)
+      r
+    else
+      r + capacity
+  }
+  override def isEmpty: Boolean = start == end
+
+  override def length: Int = size
+
+  override def apply(idx: Int): T =
+    if(idx >= size) throw new IndexOutOfBoundsException(s"accessing element at $idx in queue of size $size")
+    else data((start + idx) % data.size)
+}
+
+object MutableArrayQueue{
+  def apply[T](xs: Iterable[T]) = {
+    val r = new MutableArrayQueue[T](xs.size)
+    xs.foreach(r.enqueue)
+    r
+  }
+}
---
+++ src/test/scala/vultura/util/MutableArrayQueueTest.scala
@@ -0,0 +1,65 @@
+package vultura.util
+
+import org.specs2._
+import org.specs2.specification.Fragments
+
+/**
+ * Created by thomas on 24.06.14.
+ */
+class MutableArrayQueueTest extends Specification {
+  def queue(capacity: Int) = new MutableArrayQueue[Int](capacity)
+  override def is: Fragments =
+    "dequeue first element" ! {
+      val q = queue(2)
+      q.enqueue(1)
+      (q.dequeue === 1) and (q.isEmpty must beTrue)
+    } ^
+   "throw on empty dequeue" ! (queue(1).dequeue must throwA[NoSuchElementException]) ^
+   "dequeue two elements" ! {
+     val q = new MutableArrayQueue[Int](2)
+     q.enqueue(1)
+     q.enqueue(2)
+     (q.dequeue === 1) and (q.dequeue === 2)
+   } ^
+  "full queue must not be empty" ! {
+    val q = new MutableArrayQueue[Int](1)
+    q.enqueue(1)
+    q.isEmpty must beFalse
+  } ^
+  "empty queue must have correct size" ! (queue(2).size === 0) ^
+  "queue must report correct initial capacity" ! (queue(2).capacity === 2) ^
+  "non-empty queue must have correct size" ! {
+    val q = queue(2)
+    q.enqueue(1)
+    q.size === 1
+  } ^
+  "full queue must have correct size" ! {
+    val q = queue(2)
+    q.enqueue(1)
+    q.enqueue(2)
+    q.size === 2
+  } ^
+  "grown queue must have correct size" ! {
+    val q = queue(1)
+    q.enqueue(1)
+    q.enqueue(2)
+    q.size === 2
+  } ^
+  "grown queue must report correct capacity" ! {
+    val q = queue(1)
+    q.enqueue(1)
+    q.enqueue(1)
+    q.capacity === 3
+  } ^
+  "grow queue" ! {
+    val q = queue(1)
+    q.enqueue(1)
+    q.enqueue(2)
+    (q.dequeue === 1) and (q.dequeue === 2)
+  } ^
+  "check indexed access" ! {
+    val q = queue(1)
+    (0 until 10).foreach(q.enqueue)
+    q.toList === (0 until 10).toList
+  }
+}
