18c3013bcf3d3b8a6061076605a5a6720615ee99
Thomas Geier <thomas.geier@uni-ulm.de>
2014-09-09 10:46:55 +0200

    add tests for FactoredScheme constructors

--- src/test/scala/vultura/factor/inference/conditioned/lcbp/FactoredSchemeTest.scala
+++ src/test/scala/vultura/factor/inference/conditioned/lcbp/FactoredSchemeTest.scala
@@ -2,26 +2,36 @@ package vultura.factor.inference.conditioned.lcbp
 
 import org.specs2.Specification
 import org.specs2.specification.Fragments
-import vultura.factor.generators._
 import vultura.factor.{NormalD, Problem}
+import vultura.factor.generators._
 
 class FactoredSchemeTest extends Specification {
   val problem4v = Problem(IndexedSeq(), Array(2,2,2,2), NormalD)
   val singletonScheme4v = FactoredScheme(problem4v, (0 to 3).map(x => x -> Set(x)).toMap)
-
-  val problem4x4: Problem = grid(2,2)
-  val scheme4x4_exact = FactoredScheme(problem4x4.simplify,Map(0 -> Set(0), 1 -> Set(0), 2 -> Set(0), 3 -> Set(0))) //break the only loop
 
   override def is: Fragments =
     "constructing all assignments to a set of variables" ^
       (singletonScheme4v.allAssignmentsTo(Set(0)) === Set(Map(0->0),Map(0->1))) ^
       (singletonScheme4v.allAssignmentsTo(Set(0,1)) === Set(Map(0->0,1->0),Map(0->0,1->1),Map(0->1,1->0),Map(0->1,1->1))) ^
     p^
-    "calculating subconditions" ^
+    "calculating sub-conditions" ^
       (singletonScheme4v.subConditionsOf(Map(0->0), Set(0,1)) === Set(Map(0->0,1->0), Map(0->0, 1->1))) ^
     "allowed values under condition" ^
       (singletonScheme4v.allowedValuesUnderCondition(0,Map(0->0)) === Set(0)) ^
       (singletonScheme4v.allowedValuesUnderCondition(0,Map(1->0)) === Set(0,1)) ^
       (singletonScheme4v.allowedValuesUnderCondition(0,Map(0->1,1->0)) === Set(1)) ^
-      (scheme4x4_exact.allowedValuesUnderCondition(0, Map(0->1,1->0)) === Set(1))
+    p^
+    "maximum graphical distance constructor" ^
+      "condition only the variable itself" !
+        (FactoredScheme.withMaxDistance(Set(0),0,grid(2,2)).conditionersOf(Set(0)) === Set(0)) ^
+      "with distance 1" !
+        (FactoredScheme.withMaxDistance(Set(0),1,grid(2,2)).conditionersOf(Set(0)) === Set(0,1,2)) ^
+      "two conditioners" !
+        (FactoredScheme.withMaxDistance(Set(0,2),1,grid(3,1)).conditionersOf(Set(2)) === Set(1,2)) ^
+    p^
+    "maximum loop length constructor" ^
+      "zero loop length" !
+        (FactoredScheme.withAllLoopsOfLength(Set(0),0,grid(2,2)).conditionersOf(Set(0)) === Set(0)) ^
+      "loops of length one" !
+        (FactoredScheme.withAllLoopsOfLength(Set(0),1,grid(2,2)).conditionersOf(Set(0)) === Set(0,1,2,3))
 }
