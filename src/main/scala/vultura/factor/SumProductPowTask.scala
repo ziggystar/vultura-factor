99a868fc459513454ebb548b6710cdb8943f40f8
Thomas Geier <thomas.geier@uni-ulm.de>
2015-02-12 10:39:06 +0100

    add some tests for SumProductPowTask

--- src/main/scala/vultura/factor/SumProductPowTask.scala
+++ src/main/scala/vultura/factor/SumProductPowTask.scala
@@ -9,81 +9,97 @@ package vultura.factor
   * @param domainSizes
   * @param factorVariables A pairing of factors and their power.
   */
-case class SumProductPowTask(remainingVars: Array[Int],
-                             domainSizes: Array[Int],
-                             factorVariables: Seq[(Double,Array[Array[Int]])],
-                             ring: Ring[Double]){
-  val numFactors: Int = factorVariables.size
-  val remainSize: Int = Factor.mapMultiply(remainingVars,domainSizes)
+case class SumProductPowTask(domainSizes: Array[Int],
+                             ring: Ring[Double],
+                             remainingVars: Array[Int],
+                             factorVariables: Seq[(Double,Array[Array[Int]])]
+                             ){
+//  val numFactors: Int = factorVariables.size
+//  val remainSize: Int = Factor.mapMultiply(remainingVars,domainSizes)
+//
+//  //collect all variables
+//  val (cliqueOrdering: Array[Int], margVars: Array[Int]) = {
+//    val allVars: Array[Int] = factorVariables.flatten.toSet.toArray
+//    //reorder, so that all margVars are at beginning
+//    val mv: Array[Int] = allVars.filterNot(remainingVars.contains)
+//    (mv ++ remainingVars,mv)
+//  }
+//  val lookups: Array[Array[Int]] = factorVariables.map(Factor.buildLookup(cliqueOrdering,domainSizes,_))(collection.breakOut)
+//  val margSize: Int = Factor.mapMultiply(margVars,domainSizes)
+//  //domain sizes ordered by the appearance of variables in the clique ordering (for efficient access when counting)
+//  val cliqueDomains: Array[Int] = cliqueOrdering.map(domainSizes)
+//  val counterSize: Int = cliqueOrdering.length
+//
+//  /** Holds the temporary values that get summed, get overwritten before being read in sumProduct. */
+//  val margTemp: Array[Double] = new Array[Double](margSize)
+//  /** Holds the temporary values that get multiplied. Get written to before being read in sumProduct. */
+//  val prodTemp: Array[Double] = new Array[Double](numFactors)
+//  val counter: Array[Int] = new Array[Int](counterSize)
+//  val factorPointers: Array[Int] = new Array[Int](numFactors)
 
-  //collect all variables
-  val (cliqueOrdering: Array[Int], margVars: Array[Int]) = {
-    val allVars: Array[Int] = factorVariables.flatten.toSet.toArray
-    //reorder, so that all margVars are at beginning
-    val mv: Array[Int] = allVars.filterNot(remainingVars.contains)
-    (mv ++ remainingVars,mv)
+  final def sumProduct(factorValues: Array[Array[Array[Double]]], result: Array[Double]) {
+//    //TODO maybe the clearing is not needed
+//    SumProductTask.arrayClear(counter)
+//    SumProductTask.arrayClear(factorPointers)
+//    var remainIdx = 0
+//    var cnt = 0
+//    while(remainIdx < remainSize){
+//      var margIdx = 0
+//      while(margIdx < margSize){
+//        //increment counter
+//        val overflow = increment(cnt)
+//        cnt += 1
+//        //calculate the factor contributions
+//        //NOTE: we collect the factor values for the counter state before its update in this loop!
+//        var fi = 0
+//        while(fi < numFactors){
+//          prodTemp(fi) = factorValues(fi)(factorPointers(fi))
+//          factorPointers(fi) += lookups(fi)(overflow)
+//          fi += 1
+//        }
+//        //multiply factor values
+//        margTemp(margIdx) = ring.prodA(prodTemp)
+//        margIdx += 1
+//      }
+//      //now sum over marginalized variables for one assignment to the remaining variables
+//      result(remainIdx) = ring.sumA(margTemp)
+//      remainIdx += 1
+//    }
+    ???
   }
-  val lookups: Array[Array[Int]] = factorVariables.map(Factor.buildLookup(cliqueOrdering,domainSizes,_))(collection.breakOut)
-  val margSize: Int = Factor.mapMultiply(margVars,domainSizes)
-  //domain sizes ordered by the appearance of variables in the clique ordering (for efficient access when counting)
-  val cliqueDomains: Array[Int] = cliqueOrdering.map(domainSizes)
-  val counterSize: Int = cliqueOrdering.length
 
-  /** Holds the temporary values that get summed, get overwritten before being read in sumProduct. */
-  val margTemp: Array[Double] = new Array[Double](margSize)
-  /** Holds the temporary values that get multiplied. Get written to before being read in sumProduct. */
-  val prodTemp: Array[Double] = new Array[Double](numFactors)
-  val counter: Array[Int] = new Array[Int](counterSize)
-  val factorPointers: Array[Int] = new Array[Int](numFactors)
+//  /** Increments the counter.
+//    * Mutates the `counter` member.
+//    * @param count The current step number of the counter.
+//    * @return The counter figure where the overflow occurred.
+//    */
+//  @inline
+//  private final def increment(count: Int): Int = {
+//    var overflow = 0
+//
+//    while(overflow < counterSize){
+//      counter(overflow) += 1
+//      if(counter(overflow) == cliqueDomains(overflow)){
+//        counter(overflow) = 0
+//        overflow += 1
+//      } else {
+//        return overflow
+//      }
+//    }
+//    overflow
+//  }
+}
 
-  final def sumProduct(factorValues: IndexedSeq[Array[Double]], result: Array[Double]) {
-    //TODO maybe the clearing is not needed
-    SumProductTask.arrayClear(counter)
-    SumProductTask.arrayClear(factorPointers)
-    var remainIdx = 0
-    var cnt = 0
-    while(remainIdx < remainSize){
-      var margIdx = 0
-      while(margIdx < margSize){
-        //increment counter
-        val overflow = increment(cnt)
-        cnt += 1
-        //calculate the factor contributions
-        //NOTE: we collect the factor values for the counter state before its update in this loop!
-        var fi = 0
-        while(fi < numFactors){
-          prodTemp(fi) = factorValues(fi)(factorPointers(fi))
-          factorPointers(fi) += lookups(fi)(overflow)
-          fi += 1
-        }
-        //multiply factor values
-        margTemp(margIdx) = ring.prodA(prodTemp)
-        margIdx += 1
-      }
-      //now sum over marginalized variables for one assignment to the remaining variables
-      result(remainIdx) = ring.sumA(margTemp)
-      remainIdx += 1
-    }
-  }
-
-  /** Increments the counter.
-    * Mutates the `counter` member.
-    * @param count The current step number of the counter.
-    * @return The counter figure where the overflow occurred.
-    */
-  @inline
-  private final def increment(count: Int): Int = {
-    var overflow = 0
-
-    while(overflow < counterSize){
-      counter(overflow) += 1
-      if(counter(overflow) == cliqueDomains(overflow)){
-        counter(overflow) = 0
-        overflow += 1
-      } else {
-        return overflow
-      }
-    }
-    overflow
+object SumProductPowTask {
+  def runOnce(domains: Array[Int],
+              remaining: Array[Int],
+              ring: Ring[Double],
+              factorGroups: Seq[(Double,Seq[Factor])]): Factor = {
+    val scopes: IndexedSeq[(Double,Array[Array[Int]])] =
+      factorGroups.map { case (p, fs) => (p, fs.map(_.variables)(collection.breakOut): Array[Array[Int]])}(collection.breakOut)
+    val task = SumProductPowTask(domains, ring, remaining, scopes)
+    val result = new Array[Double](remaining.map(domains).product)
+    task.sumProduct(factorGroups.map(_._2.map(_.values)(collection.breakOut): Array[Array[Double]])(collection.breakOut),result)
+    Factor(remaining,result)
   }
 }
---
+++ src/test/scala/vultura/factor/SumProductPowTaskTest.scala
@@ -0,0 +1,34 @@
+package vultura.factor
+
+import org.specs2.Specification
+import org.specs2.specification.Fragments
+import vultura.factor.generators._
+
+import scala.util.Random
+
+class SumProductPowTaskTest extends Specification with FactorMatchers {
+  override def is: Fragments =
+    "identity operation" ! {
+      val domains: Array[Int] = Array(2, 2)
+      val scope: Array[Var] = Array(0, 1)
+      val f = expGauss().apply(scope,domains, new Random(0))
+      SumProductPowTask.runOnce(domains,scope,NormalD,Seq((1d,Seq(f)))) must beSimilarTo(f,1e-12)
+    } ^
+    "factor division by self" ! {
+      val domains: Array[Int] = Array(2, 2)
+      val scope: Array[Var] = Array(0, 1)
+      val f = expGauss().apply(scope,domains, new Random(0))
+      SumProductPowTask.runOnce(domains,scope,NormalD,Seq((1d,Seq(f)),(-1d,Seq(f)))) must beSimilarTo(Factor.maxEntropy(scope,domains,NormalD),1e-12)
+    } ^
+    "normal sum-product of two factors" ! {
+      val domains: Array[Int] = Array(2, 2, 2)
+      val scope1: Array[Var] = Array(0, 1)
+      val scope2: Array[Var] = Array(1, 2)
+      val f1 = expGauss().apply(scope1,domains,new Random(0))
+      val f2 = expGauss().apply(scope2,domains,new Random(1))
+      SumProductPowTask.runOnce(domains,Array(1),NormalD,Seq((1d,Seq(f1,f2)))) must
+        beSimilarTo(Factor.multiplyRetain(NormalD)(domains)(Array(f1,f2),Array(1)),1e-12)
+    }
+
+
+}
