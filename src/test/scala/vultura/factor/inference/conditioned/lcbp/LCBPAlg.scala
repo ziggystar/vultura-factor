345ec2fa711be7075ebd43f0a4aece77812b7524
Thomas Geier <thomas.geier@uni-ulm.de>
2015-02-17 17:28:43 +0100

    minor refactor lcbp abstraction

--- src/test/scala/vultura/factor/inference/conditioned/lcbp/LCBPAlg.scala
+++ src/test/scala/vultura/factor/inference/conditioned/lcbp/LCBPAlg.scala
@@ -5,20 +5,20 @@ import vultura.factor.inference.MargParI
 import vultura.factor.inference.calibration.LBP
 
 trait LCBPAlg[X] {
-  def inferWithScheme(p: Problem, s: FactoredScheme, tol: Double = 1e-12, maxIter: Int = 10000): (MargParI, Boolean, X)
+  def inferWithScheme(s: FactoredScheme, tol: Double = 1e-12, maxIter: Int = 10000): (MargParI, Boolean, X)
 }
 
 /** The old implementation, using exact meta inference with counting. */
 object OldLCBP extends LCBPAlg[LCBP] {
-  override def inferWithScheme(p: Problem, s: FactoredScheme, tol: Double = 1e-12, maxIter: Int = 10000): (MargParI, Boolean, LCBP) = {
-    val lcbp = new LCBP(p,s.toGScheme,tol,maxIter)
+  override def inferWithScheme(s: FactoredScheme, tol: Double = 1e-12, maxIter: Int = 10000): (MargParI, Boolean, LCBP) = {
+    val lcbp = new LCBP(s.problem,s.toGScheme,tol,maxIter)
     (lcbp,lcbp.calibrator.isConverged,lcbp)
   }
 }
 
 /** The new lcbp implementation, using exact meta inference with junction tree. */
 object LCBP_G_Exact extends LCBPAlg[LCBPGeneral] {
-  override def inferWithScheme(p: Problem, s: FactoredScheme, tol: Double, maxIter: Int): (MargParI, Boolean, LCBPGeneral) = {
+  override def inferWithScheme(s: FactoredScheme, tol: Double, maxIter: Int): (MargParI, Boolean, LCBPGeneral) = {
     val lcbp = new LCBPGeneral(s, maxUpdates = maxIter, tol = tol)
     (lcbp,lcbp.calibrator.isConverged,lcbp)
   }
@@ -26,7 +26,7 @@ object LCBP_G_Exact extends LCBPAlg[LCBPGeneral] {
 
 /** The new lcbp implementation, using bp as meta inference as plugin for general solver. */
 object LCBP_G_BP extends LCBPAlg[LCBPGeneral] {
-  override def inferWithScheme(p: Problem, s: FactoredScheme, tol: Double, maxIter: Int): (MargParI, Boolean, LCBPGeneral) = {
+  override def inferWithScheme(s: FactoredScheme, tol: Double, maxIter: Int): (MargParI, Boolean, LCBPGeneral) = {
     val lcbp = new LCBPGeneral(s, inferer = px => LBP.infer(px,maxIter,tol),maxUpdates = maxIter, tol = tol)
     (lcbp,lcbp.calibrator.isConverged,lcbp)
   }
@@ -34,7 +34,7 @@ object LCBP_G_BP extends LCBPAlg[LCBPGeneral] {
 
 /** The native lcbp_bp implementation using a native bp implementation for meta inference. */
 object LCBP_BP extends LCBPAlg[LcbpMetaBP] {
-  override def inferWithScheme(p: Problem, s: FactoredScheme, tol: Double, maxIter: Int): (MargParI, Boolean, LcbpMetaBP) = {
+  override def inferWithScheme(s: FactoredScheme, tol: Double, maxIter: Int): (MargParI, Boolean, LcbpMetaBP) = {
     val lcbp = new LcbpMetaBP(s,maxIter,tol)
     (lcbp,lcbp.calibrator.isConverged,lcbp)
   }
