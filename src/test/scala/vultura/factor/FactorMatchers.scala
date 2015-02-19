5ee0916c9502c00f5e6c1a5a11bed31fc5ecc79d
Thomas Geier <thomas.geier@uni-ulm.de>
2015-02-19 11:46:23 +0100

    refactor FactorMatchers

--- src/test/scala/vultura/factor/FactorMatchers.scala
+++ src/test/scala/vultura/factor/FactorMatchers.scala
@@ -35,62 +35,42 @@ trait FactorMatchers {
       result(
         math.abs(obtainedZ - otherZ) < tol,
         s"${t.description} has same Z as exact inference",
-        s"${t.description} has different Z compared to exact inference (got $obtainedZ, provided is $otherZ)",
+        s"${t.description} has different Z compared to exact inference (got $obtainedZ, ${t.description} is $otherZ)",
         t
       )
     }
   }
-  def haveSameLogZ(other: ParFunI, tol: Double): Matcher[ParFunI] = new Matcher[ParFunI]{
-    def apply[S <: ParFunI](t: Expectable[S]): MatchResult[S] = {
-      val obtainedZ: Double = t.value.logZ
-      val otherZ = other.logZ
-      result(
-        math.abs(obtainedZ - otherZ) < tol,
-        s"${t.description} has same Z as exact inference",
-        s"${t.description} has different Z compared to exact inference (got $obtainedZ, provided is $otherZ)",
-        t
-      )
-    }
-  }
+  def haveSameLogZ(other: ParFunI, tol: Double): Matcher[ParFunI] = haveSameLogZ(_ => other, tol)
 
 
   def haveExactMarginals(tol: Double = 1e-9) = haveSameMarginals(new JunctionTree(_),tol)
   def haveExactZ(tol: Double = 1e-9) = haveSameLogZ(new JunctionTree(_),tol)
 
-  def haveSameMarginals(inference: Problem => MarginalI, tol: Double): Matcher[MarginalI] = new Matcher[MarginalI]{
+  def haveSameMarginals(inference: Problem => MarginalI, tol: Double, logDomain: Boolean = true): Matcher[MarginalI] = new Matcher[MarginalI]{
     def apply[S <: MarginalI](t: Expectable[S]): MatchResult[S] = {
       val p = t.value.problem
       val other = inference(p)
+
+      def marg(mi: MarginalI,v: Int): Factor = if(logDomain) mi.logVariableBelief(v) else mi.decodedVariableBelief(v)
+
       val error: Option[(Int, Double)] = p.variables.map(v =>
-        v -> (other.variableBelief(v).values zip t.value.variableBelief(v).values)
+        v -> (marg(other,v).values zip marg(t.value,v).values)
           .map{case (x,y) => math.abs(x-y)}
           .max
       ).find(_._2 > tol)
+
+      val domainString: String = if(logDomain) "log" else "normal"
+
       result(
         error.isEmpty,
         s"$t has exact marginals",
-        s"${t.description} differs in marginals by ${error.get._2} for variable ${error.get._1}",
+        s"${t.description} differs in marginals ($domainString encoded) by ${error.get._2} for variable ${error.get._1}",
         t
       )
     }
   }
 
-  def haveSameMarginals(other: MarginalI, tol: Double): Matcher[MarginalI] = new Matcher[MarginalI]{
-    def apply[S <: MarginalI](t: Expectable[S]): MatchResult[S] = {
-      val p = t.value.problem
-      val error: Option[(Int, Double)] = p.variables.map(v =>
-        v -> (other.variableBelief(v).values zip t.value.variableBelief(v).values)
-          .map{case (x,y) => math.abs(x-y)}
-          .max
-      ).find(_._2 > tol)
-      result(
-        error.isEmpty,
-        "has exact marginals",
-        s"${t.description} differs in marginals by ${error.get._2} for variable ${error.get._1}",
-        t
-      )
-    }
-  }
+  def haveSameMarginals(other: MarginalI, tol: Double): Matcher[MarginalI] = haveSameMarginals(_ => other, tol)
 
   def beCloseTo(ref: Seq[Double], tol: Double = 1e-12): Matcher[Seq[Double]] = new Matcher[Seq[Double]]{
     override def apply[S <: Seq[Double]](t: Expectable[S]): MatchResult[S] = result(
