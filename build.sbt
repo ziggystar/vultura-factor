14c2e22ff2f40175551a5a51b0367a2cec62d949
Thomas Geier <thomas.geier@uni-ulm.de>
2015-03-02 11:05:33 +0100

    fix merge accident; 22.3.0

--- build.sbt
+++ build.sbt
@@ -15,8 +15,9 @@ organization := "de.uni-ulm"
 22.0.0: fix different LCBP implementations
 22.1.0: Add fully factorized CBP inference; some refactorings
 22.2.0: add interactions on second layer of two-tier grid problem generator
+22.3.0: fix merge/commit accident where FullyConditionedBP from 22.1.0 was missing from 22.2.0
 */
-version := "22.2.0"
+version := "22.3.0"
 
 homepage := Some(url("http://www.uni-ulm.de/in/ki/staff/thomas-geier.html"))
 
---
+++ src/main/scala/vultura/factor/inference/conditioned/FullyConditionedBP.scala
@@ -0,0 +1,27 @@
+package vultura.factor.inference.conditioned
+
+import vultura.factor.{Factor, LogD, Problem}
+import vultura.factor.inference.MargParI
+import vultura.factor.inference.calibration.{LBP, BPResult}
+
+
+case class FullyConditionedBP(problem: Problem,
+                              conditionVariables: Set[Int],
+                              maxIterations: Int = 100000,
+                              tol: Double = 1e-12) extends MargParI {
+  val conditions: IndexedSeq[Map[Int, Int]] = conditionVariables.foldLeft(Seq(Map.empty[Int,Int])){
+    case (pcs,nv) => pcs.flatMap(pc => (0 until problem.domains(nv)).map(value => pc + (nv -> value)))
+  }.toIndexedSeq
+
+  val results: IndexedSeq[(BPResult, Boolean, Long)] = conditions.map(c => LBP.inferWithStats(problem.condition(c)))
+  val weights: Array[Double] = LogD.decode(LogD.normalize(results.map(_._1.logZ)(collection.breakOut)))
+  override def variableBelief(vi: Int): Factor = Factor.linearCombination(weights, results.map(_._1.variableBelief(vi)), problem.ring)
+  override def Z: Double = problem.ring.encode(Array(logZ)).head
+
+  /** @return Natural logarithm of partition function. */
+  override def logZ: Double = LogD.sumA(results.map(_._1.logZ)(collection.breakOut))
+
+  val isConverged: Boolean = results.forall(_._2)
+  val maxSteps: Long = results.map(_._3).max
+  val totalSteps: Long = results.map(_._3).sum
+}
