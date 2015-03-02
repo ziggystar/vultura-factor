a06f192115ea22b6bc4a84edf97483266359a399
Thomas Geier <thomas.geier@uni-ulm.de>
2015-03-02 10:44:30 +0100

    add interactions on higher tier for two-layer grid

--- src/main/scala/vultura/factor/generators/HierarchicalGrid.scala
+++ src/main/scala/vultura/factor/generators/HierarchicalGrid.scala
@@ -2,6 +2,7 @@ package vultura.factor.generators
 
 import vultura.factor.{Problem, LabeledPS, FactorGenerator}
 
+import scala.collection.immutable
 import scala.util.Random
 
 /** Two-Layer hierarchical grid problems.
@@ -9,18 +10,21 @@ import scala.util.Random
 case class GridH2(cellWidth: Int = 3, cellHeight: Int = 3, cellRows: Int = 3, cellCols: Int = 3) {
   type N = (Int,Int,Int)
   type F = (N,N)
-  val labeledPS: LabeledPS[(Int,Int,Int),((Int,Int,Int),(Int,Int,Int))] = {
+  val labeledPS: LabeledPS[N,F] = {
     val lwidth = cellWidth * cellCols
     val lheight = cellHeight * cellRows
 
     def isValidLowerNode(ln: N) = ln._3 == 0 &&
       (0 until lwidth).contains(ln._1) && (0 until lheight).contains(ln._2)
 
-    val lowerGridScopes = for {
+    val lowerGridScopes: IndexedSeq[(F, Set[N])] = for {
       xl <- 0 until lwidth
       yl <- 0 until lheight
       n <- Seq((xl,yl+1,0),(xl + 1,yl,0)).filter(isValidLowerNode)
-    } yield ((xl,yl,0),n) -> Set((xl,yl,0),n)
+    } yield {
+      val lowerNode: N = (xl, yl, 0)
+      (lowerNode, n) -> Set(lowerNode, n)
+    }
 
     val lowerGrid: LabeledPS[N,F] = LabeledPS.empty.addFactors(lowerGridScopes:_*)
     def isHighLowConnection(hn: N, ln: N): Boolean =
@@ -32,15 +36,28 @@ case class GridH2(cellWidth: Int = 3, cellHeight: Int = 3, cellRows: Int = 3, ce
     val higherNodes: IndexedSeq[N] = for(hx <- 0 until cellCols; hy <- 0 until cellRows) yield (hx,hy,1)
     val twoTierProblem = lowerGrid.addFactors(
       higherNodes.flatMap(hn => lowerNeighbours(hn).map(ln => (hn,ln) -> Set(hn,ln))):_*)
-    twoTierProblem
+
+    def isValidUpperNode(n: N) = n._3 == 1 && (0 until cellCols).contains(n._1) && (0 until cellRows).contains(n._2)
+
+    val highInteractions: IndexedSeq[(F,Set[N])] = for{
+      hx <- 0 until cellCols
+      hy <- 0 until cellRows
+      n <- Seq((hx,hy+1,1),(hx + 1,hy,1)).filter(isValidUpperNode)
+    } yield {
+      val hn = (hx,hy,1)
+      (hn,n) -> Set(hn,n)
+    }
+
+    twoTierProblem.addFactors(highInteractions:_*)
   }
 
-  def generate(domainSize: Int, lowerPot: FactorGenerator, upperPot: FactorGenerator, random: Random): Problem =
+  def generate(domainSize: Int, lowerPot: FactorGenerator, interPot: FactorGenerator, upperPot: FactorGenerator, random: Random): Problem =
     labeledPS.generateProblem(
       domainSize = _=> domainSize,
       potGen = {
-        case ((_,_,0),_) => lowerPot
-        case ((_,_,1),_) => upperPot
+        case ((_,_,0),(_,_,0)) => lowerPot
+        case ((_,_,1),(_,_,0)) => interPot
+        case ((_,_,1),(_,_,1)) => upperPot
       },
       random = random
     )
