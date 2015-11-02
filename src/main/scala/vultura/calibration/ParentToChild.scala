01da802a4e89b8c43d3447c608852587e13ab793
Thomas Geier <thomas.geier@uni-ulm.de>
2015-11-02 17:39:12 +0100

    add stub for new ParentToChild algorithm

---
+++ src/main/scala/vultura/calibration/ParentToChild.scala
@@ -0,0 +1,46 @@
+package vultura.calibration
+
+import vultura.factor.inference.{RegionBeliefs, VariationalResult}
+import vultura.factor.{ProblemStructure, Problem}
+import vultura.factor.inference.gbp.RegionGraph
+
+/** Implementation of the parent-to-child algorithm to optimize region graph free energies.
+  * What about
+  *  - redundancy?
+  */
+case class ParentToChild(rg: RegionGraph, parameters: Problem) extends CalProblem
+with ResultBuilder[RegionBeliefs[RegionGraph#Region] with VariationalResult]{
+  override type N = P2C
+
+  val ps: ProblemStructure = rg.problemStructure
+  type VI = ps.VI
+  type FI = ps.FI
+  type R = rg.Region
+
+  case class P2C(parent: R, child: R) extends Node {
+    /** Type of dependencies .*/
+    override type D = P2C
+
+    lazy val variables: Array[VI] = rg.variablesOf(child).toArray.sorted
+
+    /** Size of the array required to store the state of this edge. */
+    final override def arraySize: Int = variables.map(ps.domains).product
+
+    override def dependencies: IndexedSeq[P2C] = ???
+
+    /**
+     * - first parameter: `zip`s with `dependencies`.
+     * - second parameter: Result of computation shall be stored here. Content of result is garbage.
+     */
+    override def compute(ins: Array[IR], result: IR): Unit = ???
+  }
+
+  /** Constructs a new initial value for each edge. */
+  override def initializer: N => IR = ???
+
+  /** The set of nodes defined by this problem. */
+  override def nodes: Set[N] = ???
+
+
+  override def buildResult(valuation: (P2C) => IR): RegionBeliefs[RegionGraph#Region] with VariationalResult = ???
+}
---
+++ src/test/scala/vultura/calibration/PTCTest.scala
@@ -0,0 +1,20 @@
+package vultura.calibration
+
+import org.specs2.mutable.Specification
+import vultura.factor.inference.calibration.{LBP, BPResult}
+import vultura.factor.inference.gbp.RegionGraph
+import vultura.factor.{FactorMatchers, LogD, Problem}
+import vultura.factor.generators._
+
+class PTCTest extends Specification with FactorMatchers {
+  val p1: Problem = grid(5,5,4).simplify.toRing(LogD)
+
+  "compare PTC on bethe RG with ordinary LBP result" >> {
+    val regularBPResult: BPResult = LBP.infer(p1,tol=1e-15)
+
+    val (ptcResult, status) = Calibrator.calibrate(ParentToChild(RegionGraph.betheRG(p1),p1),tol=1e-15)
+
+    status.isConverged and (ptcResult must haveSameMarginals(LBP.infer(_,tol=1e-15),1e-12))
+  }
+
+}
