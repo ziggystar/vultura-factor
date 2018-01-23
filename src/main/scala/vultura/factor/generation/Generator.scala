1783e7f7f659d5591b7de51e70994aaf4e9713da
Thomas Vultura <ziggystar@gmx.de>
2018-01-23 19:01:04 +0100

    add `filter` method to Generator

--- src/main/scala/vultura/factor/generation/Generator.scala
+++ src/main/scala/vultura/factor/generation/Generator.scala
@@ -1,7 +1,6 @@
 package vultura.factor.generation
 
 import org.apache.commons.math3.distribution.GammaDistribution
-import org.apache.commons.math3.random.JDKRandomGenerator
 
 import scala.util.Random
 
@@ -10,29 +9,20 @@ import scala.util.Random
 trait Generator[+A] { outer =>
   def generate(r: Random): A
   def withSeed(s: Long = 0L): A = generate(new Random(s))
-  def map[B](f: A => B): Generator[B] = new Generator[B]{
-    override def generate(r: Random): B = f(outer.generate(r))
+  def map[B](f: A => B): Generator[B] = (r: Random) => f(outer.generate(r))
+  def flatMap[B](f: A => Generator[B]): Generator[B] = (r: Random) => f(outer.generate(r)).generate(r)
+  def withFilter(p: A => Boolean): Generator[A] = (r: Random) => {
+    var res: A = null.asInstanceOf[A]
+    do {
+      res = outer.generate(r)
+    } while (!p(res))
+    res
   }
-  def flatMap[B](f: A => Generator[B]): Generator[B] = new Generator[B]{
-    override def generate(r: Random): B = f(outer.generate(r)).generate(r)
-  }
-  def withFilter(p: A => Boolean): Generator[A] = new Generator[A] {
-    def generate(r: Random): A = {
-      var res: A = null.asInstanceOf[A]
-      do {
-        res = outer.generate(r)
-      } while(!p(res))
-      res
-    }
-  }
-
-  def replicate(n: Int): Generator[IndexedSeq[A]] = Generator(r => IndexedSeq.fill(n)(this.generate(r)))
+  def filter(p: A => Boolean): Generator[A] = withFilter(p)
 }
 
 object Generator {
-  def apply[A](f: Random => A): Generator[A] = new Generator[A]{
-    override def generate(r: Random): A = f(r)
-  }
+  def apply[A](f: Random => A): Generator[A] = (r: Random) => f(r)
   def draw[A](as: Seq[A]): Generator[A] = (r: Random) => as(r.nextInt(as.size))
   def seq[X](xs: Seq[Generator[X]]): Generator[Seq[X]] = Generator(r => xs.map(_.generate(r)))
   def gaussian(mean: Double = 0d, sd: Double = 1d): Generator[Double] = Generator(_.nextGaussian() * sd + mean)
