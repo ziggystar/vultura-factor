29445c7c30a2031b9a3d3a33253f992c3e995cd6
Thomas Vultura <ziggystar@gmx.de>
2018-01-24 19:18:53 +0100

    more Ring tests

--- src/test/scala/vultura/factor/NormalDTest.scala
+++ src/test/scala/vultura/factor/NormalDTest.scala
@@ -1,8 +1,15 @@
 package vultura.factor
 
-import org.specs2._
+import org.specs2.mutable._
 
 class NormalDTest extends Specification {
-  override def is =
-    "log expectation with zeros" ! (NormalD.logExpectation(Array(0d,1), Array(0d,1)) === 0d)
+  "log expectation" should {
+    "with zeros" ! (NormalD.logExpectation(Array(0d, 1), Array(0d, 1)) === 0d)
+    "some test" ! (NormalD.logExpectation(Array(0.2,0.8), Array(1,2)) === 0.2 * math.log(1) + 0.8 * math.log(2))
+    "of constant zero should yield -Inf" ! (NormalD.logExpectation(Array(0,0),Array(0,0)) === Double.NegativeInfinity)
+  }
+  "expectation" should {
+    "test 1" ! (NormalD.expectation(Array(0d,1d),Array(0.3,0.7)) === 0.7)
+    "must perform normalization" ! (NormalD.expectation(Array(1,2),Array(3,4)) must beCloseTo(0.3333333 * 3 + 0.66666666 * 4, 1e-3))
+  }
 }
